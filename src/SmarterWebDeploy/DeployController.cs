3a785b5cd464b54bf59c3c61994c02a7a35c8d0f
Michael Kennedy <mikeckennedy@gmail.com>
2014-06-05 16:29:51 -0700

    Docs

--- src/SmarterWebDeploy/DeployController.cs
+++ src/SmarterWebDeploy/DeployController.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace SmarterWebDeploy
 {
-    public class DeployController : Controller
-    {
+	public class DeployController : Controller
+	{
 		public ActionResult Complete(string id)
 		{
 			try
@@ -25,5 +22,5 @@ namespace SmarterWebDeploy
 				return Content("<br />Completed with errors:\r\n<br />\r\n<br />" + x.ToString().Replace("\n", "\n<br />"));
 			}
 		}
-    }
+	}
 }
